1f72031be05bc4751bf90672df45c0be508461de
Jaka Maver <jm18457@gmail.com>
2024-07-17 20:02:38 +0200

    feat: implement

--- markdown_to_anki.py
+++ markdown_to_anki.py
@@ -7,6 +7,9 @@ import genanki
 from bs4 import BeautifulSoup
 
 
+# This function parses a markdown file and returns a list of sections.
+# Each section will get its own Anki note.
+# The tuple (title, body) represents a section.
 def parse_markdown_file(file_path):
     with open(file_path, 'r') as file:
         text = file.read()
@@ -14,21 +17,19 @@ def parse_markdown_file(file_path):
     soup = BeautifulSoup(html, 'html.parser')
 
     sections = []
-    current_h1_tag = ''
     current_h2_tag = ''
     current_body = ''
 
     # Replace the src attribute of img tags with just the filename
+    # This is needed because Anki requires all media files to be in the same directory as the deck
     for img in soup.find_all('img'):
         img['src'] = os.path.basename(img['src'])
 
     for tag in soup:
-        if tag.name == 'h1':
-            current_h1_tag = tag.text
-        elif tag.name == 'h2':
+        if tag.name == 'h2':
             # If there's a previous h2 tag, append it and its body to the sections
             if current_h2_tag:
-                sections.append((current_h1_tag, current_h2_tag, current_body))
+                sections.append((current_h2_tag, current_body))
             # Start a new section
             current_h2_tag = tag.text
             current_body = ''
@@ -38,13 +39,14 @@ def parse_markdown_file(file_path):
 
     # Append the last section
     if current_h2_tag:
-        sections.append((current_h1_tag, current_h2_tag, current_body))
+        sections.append((current_h2_tag, current_body))
 
     return sections
 
 
 def main():
     # Delete 'output' dir if it exists
+    # This is needed to clean up the directory before creating a new deck
     if os.path.exists('output'):
         shutil.rmtree('output')
 
@@ -52,6 +54,11 @@ def main():
     os.makedirs('output')
 
     # Copy all .png images from 'designdeck/res' to 'output' directory
+    # Anki requires all media files to be in the same directory as the deck.
+    # We will create a new deck with the images in the 'output' directory.
+    # For more info:
+    #   - https://github.com/kerrickstaley/genanki/blob/main/README.md
+    #   - https://gist.github.com/jlumbroso/e17abff02d89be04240072191af09ab2
     for file in glob.glob('designdeck/res/*.png'):
         shutil.copy(file, 'output')
 
@@ -59,7 +66,7 @@ def main():
     deck = genanki.Deck(
         deck_id=2059400110,  # This is a random ID for the deck
         name='Design Deck',
-        description='Anki deck generated from markdown https://github.com/teivah/designdeck?tab=readme-ov-file.',
+        description='Anki deck generated from https://github.com/teivah/designdeck?tab=readme-ov-file.',
     )
 
     model = genanki.Model(
@@ -69,6 +76,7 @@ def main():
             {'name': 'Front'},
             {'name': 'Back'},
         ],
+        # Templates and CSS are copied from the "Basic" model in Anki
         templates=[
             {
                 'name': 'Card 1',
@@ -79,15 +87,18 @@ def main():
         css=".card {font-family: arial; font-size: 20px; color: black; background-color: white;}"
     )
 
+    # Get a list of all markdown files that will be converted to Anki notes
     markdown_files = [f for f in os.listdir('designdeck') if
                       f.endswith('.md') and f != 'README.md' and f != 'LICENSE.md']
+
     for markdown_file in markdown_files:
+        name_without_extension = os.path.splitext(markdown_file)[0]
         sections = parse_markdown_file(os.path.join('designdeck', markdown_file))
-        for tag, title, body in sections:
+        for title, body in sections:
             note = genanki.Note(
                 model=model,
                 fields=[title, body],
-                tags=[tag],
+                tags=[name_without_extension],
             )
             deck.add_note(note)
 
